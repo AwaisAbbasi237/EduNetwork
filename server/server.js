b2fb2a92b3539a295fc2052eee14240b39e1bbbc
unknown <awaisbinsaqib002@gmail.com>
2021-03-02 23:20:18 +0500

    Code for whiteboard module. Not complete yet.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const { Socket } = require('dgram');
+
+var app = require ('express')();
+var http =require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket)=>{
+    console.log('User is Online');
+
+    socket.on('canvas-data', (data)=>{
+        socket.broadcast.emit('canvas-data', data);
+    })
+})
+
+var server_port=process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("started on : "+ server_port);
+
+})
